4c2f753d11ad63444d28cc44ba3c642145cda876
Dror Ben-Gai <dror@snyk.io>
2017-10-18 14:48:55 +0300

    feat: supporting args as an array

---
+++ test/functional/sbt-plugin.test.js
@@ -0,0 +1,24 @@
+var test = require('tap-only');
+var plugin = require('../../lib').__tests;
+
+test('check build args with array', function (t) {
+  var result = plugin.buildArgs(null, null, [
+    '-Paxis',
+    '-Pjaxen',
+  ]);
+  t.deepEqual(result, [
+    'dependency-tree',
+    '-Paxis',
+    '-Pjaxen',
+  ]);
+  t.end();
+});
+
+test('check build args with string', function (t) {
+  var result = plugin.buildArgs(null, null, '-Paxis -Pjaxen');
+  t.deepEqual(result, [
+    'dependency-tree',
+    '-Paxis -Pjaxen',
+  ]);
+  t.end();
+});
