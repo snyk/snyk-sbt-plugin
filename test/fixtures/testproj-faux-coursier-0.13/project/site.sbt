f56e943bea26ffe1f838f8e707a46f51d365ad75
Simon Hildrew <simon@hildrew.net>
2019-02-28 00:49:46 +0000

    feat: add coursier support
    as it's increasingly common and the bundled `coursierDependencyGraph`
    means there is no need to pollute projects with `sbt-dependency-graph`.

---
+++ test/fixtures/testproj-faux-coursier-0.13/project/site.sbt
@@ -0,0 +1,3 @@
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.8.2")
+// this is here, but disabled
+//addSbtPlugin("io.get-coursier" % "sbt-coursier" % "1.0.3")
--- test/system/plugin.test.js
+++ test/system/plugin.test.js
@@ -4,9 +4,9 @@ var sinon = require('sinon');
 var plugin = require('../../lib');
 var subProcess = require('../../lib/sub-process');
 
-test('run inspect()', function (t) {
+test('run inspect() on 0.13', function (t) {
   return plugin.inspect(path.join(__dirname, '..', 'fixtures'),
-    'testproj/build.sbt')
+    'testproj-0.13/build.sbt')
     .then(function (result) {
       t.equal(result.package
         .dependencies['axis:axis']
@@ -17,9 +17,48 @@ test('run inspect()', function (t) {
     });
 });
 
-test('run inspect() with no sbt plugin', function (t) {
+test('run inspect() on 1.2.8', function (t) {
+  return plugin.inspect(path.join(__dirname, '..', 'fixtures'),
+    'testproj-1.2.8/build.sbt')
+    .then(function (result) {
+      t.equal(result.package
+        .dependencies['axis:axis']
+        .dependencies['axis:axis-jaxrpc']
+        .dependencies['org.apache.axis:axis-jaxrpc'].version,
+      '1.4',
+      'correct version found');
+    });
+});
+
+test('run inspect() with coursier on 0.13', function (t) {
+  return plugin.inspect(path.join(__dirname, '..', 'fixtures'),
+    'testproj-coursier-0.13/build.sbt')
+    .then(function (result) {
+      t.equal(result.package
+        .dependencies['axis:axis']
+        .dependencies['axis:axis-jaxrpc']
+        .dependencies['org.apache.axis:axis-jaxrpc'].version,
+      '1.4',
+      'correct version found');
+    });
+});
+
+test('run inspect() with coursier on 1.2.8', function (t) {
+  return plugin.inspect(path.join(__dirname, '..', 'fixtures'),
+    'testproj-coursier-1.2.8/build.sbt')
+    .then(function (result) {
+      t.equal(result.package
+        .dependencies['axis:axis']
+        .dependencies['axis:axis-jaxrpc']
+        .dependencies['org.apache.axis:axis-jaxrpc'].version,
+      '1.4',
+      'correct version found');
+    });
+});
+
+test('run inspect() with no sbt plugin on 0.13', function (t) {
   return plugin.inspect(path.join(
-    __dirname, '..', 'fixtures', 'testproj-noplugin'),
+    __dirname, '..', 'fixtures', 'testproj-noplugin-0.13'),
   'build.sbt')
     .then(function () {
       t.fail('should not be reached');
@@ -30,10 +69,24 @@ test('run inspect() with no sbt plugin', function (t) {
     });
 });
 
+test('run inspect() with commented out coursier on 0.13', function (t) {
+  return plugin.inspect(path.join(
+    __dirname, '..', 'fixtures', 'testproj-faux-coursier-0.13'),
+  'build.sbt')
+    .then(function (result) {
+      t.equal(result.package
+        .dependencies['axis:axis']
+        .dependencies['axis:axis-jaxrpc']
+        .dependencies['org.apache.axis:axis-jaxrpc'].version,
+      '1.4',
+      'correct version found');
+    });
+});
+
 test('run inspect() with failing `sbt` execution', function (t) {
   stubSubProcessExec(t);
   return plugin.inspect(path.join(__dirname, '..', 'fixtures'),
-    'testproj/build.sbt')
+    'testproj-0.13/build.sbt')
     .then(function () {
       t.fail('should not be reached');
     })
