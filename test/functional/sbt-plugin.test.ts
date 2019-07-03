5797b36b871641ca40ee177218cd3c37d87b896d
Daniel Kontorovskyi <dan.kontorovskyy@gmail.com>
2019-07-03 14:39:12 +0300

    test: add tests for buildArgs

--- test/functional/sbt-plugin.test.ts
+++ test/functional/sbt-plugin.test.ts
@@ -48,3 +48,33 @@ test('check build args with string for coursier', (t) => {
   ]);
   t.end();
 });
+
+test('check build args with string for snykRenderTree', (t) => {
+  const result = plugin.buildArgs('-Paxis -Pjaxen', false, true);
+  t.deepEqual(result, [
+    '"-Dsbt.log.noformat=true\"',
+    '-Paxis -Pjaxen',
+    'snykRenderTree',
+  ]);
+  t.end();
+});
+
+test('check build args with string for coursier and not snykRenderTree', (t) => {
+  const result = plugin.buildArgs('-Paxis -Pjaxen', true, false);
+  t.deepEqual(result, [
+    '"-Dsbt.log.noformat=true\"',
+    '-Paxis -Pjaxen',
+    'coursierDependencyTree',
+  ]);
+  t.end();
+});
+
+test('check build args with string for not coursier and not snykRenderTree', (t) => {
+  const result = plugin.buildArgs('-Paxis -Pjaxen', false, false);
+  t.deepEqual(result, [
+    '"-Dsbt.log.noformat=true\"',
+    '-Paxis -Pjaxen',
+    'dependencyTree',
+  ]);
+  t.end();
+});
