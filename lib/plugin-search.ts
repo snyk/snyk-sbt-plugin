9598283d5ac41058d681c5ebb15ec187d28235c3
mika-bar <mika.bar@snyk.io>
2021-08-25 17:50:12 +0300

    fix: check global plugins installed
    Add a new function to check globally installed plugins.
    Refactor code away from index.ts into separate files (constants.ts, plugin-search.ts and
    version.ts).
    Test each of these files functionally.
    
    When checking if a plugin is installed, we now also check homedir .sbt folders.

---
+++ lib/plugin-search.ts
@@ -0,0 +1,60 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as os from 'os';
+import * as semver from 'semver';
+import { getSbtVersion } from './version';
+
+export async function isPluginInstalled(
+  root: string,
+  targetFile: string,
+  plugin: string,
+): Promise<boolean> {
+  const searchGlobally = await searchGlobalFiles(root, targetFile, plugin);
+  return searchGlobally || searchProjectFiles(root, targetFile, plugin);
+}
+
+// search project and project/project relative to the root
+function searchProjectFiles(root: string, targetFile: string, plugin: string): boolean {
+  const basePath = path.dirname(path.resolve(root, targetFile));
+  const sbtFileList = sbtFiles(path.join(basePath, 'project'))
+  .concat(sbtFiles(path.join(basePath, 'project', 'project')));
+  const searchResults = sbtFileList.map ((file) => {
+    return searchWithFs(file, plugin);
+  });
+  return searchResults.filter(Boolean).length > 0;
+}
+
+// search globally installed plugins (~/.sbt)
+async function searchGlobalFiles(root: string, targetFile: string, plugin: string): Promise<boolean> {
+  const homedir = os.homedir();
+  const sbtVersion = await getSbtVersion(root, targetFile);
+  // https://www.scala-sbt.org/1.x/docs/Using-Plugins.html#Global+plugins
+  const pluginsPath = semver.lt(sbtVersion, '1.0.0') ?
+    path.join(homedir, '.sbt', '0.13', 'plugins') :
+    path.join(homedir, '.sbt', '1.0', 'plugins');
+  const sbtFileList = sbtFiles(pluginsPath);
+  const searchResults = sbtFileList.map ((file) => {
+    return searchWithFs(file, plugin);
+  });
+  return searchResults.filter(Boolean).length > 0;
+}
+
+// provide a list of .sbt files in the specified directory
+function sbtFiles(basePath) {
+  if (fs.existsSync(basePath) && fs.lstatSync(basePath).isDirectory()) {
+    return fs
+      .readdirSync(basePath)
+      .filter((fileName) => {
+        return path.extname(fileName) === '.sbt';
+      })
+      .map((file) => {
+        return path.join(basePath, file);
+      });
+  }
+  return [];
+}
+
+function searchWithFs(filename, word) {
+  const buffer = fs.readFileSync(filename);
+  return buffer.indexOf(word) > -1;
+}
---
+++ lib/version.ts
@@ -0,0 +1,47 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as subProcess from './sub-process';
+
+import * as debugModule from 'debug';
+const debug = debugModule('snyk-sbt-plugin');
+
+export async function getSbtVersion(root: string, targetFile: string): Promise<string> {
+  try {
+    let buildPropsPath = path.join(
+      root,
+      path.dirname(targetFile),
+      'project/build.properties',
+      );
+    if (!fs.existsSync(buildPropsPath)) {
+        // NOTE(alexmu): We've seen this fail with the wrong path.
+        // If the path we derived above doesn't exist, we try to build a more sensible one.
+        // targetFile could be a path, so we need to call resolve()
+        const resolvedPath = path.resolve(root, targetFile);
+        const targetFilePath = path.dirname(resolvedPath);
+        buildPropsPath = path.resolve(targetFilePath, 'project/build.properties');
+      }
+    return fs
+      .readFileSync(buildPropsPath, 'utf-8')
+      .split('\n') // split into lines
+      .find((line) => !!line.match(/sbt\.version\s*=/))! // locate version line
+      .split(/=\s*/)[1]
+      .trim(); // return only the version
+  } catch (err) {
+    debug('Failed to get sbt version from project/build.properties' + err.message);
+  }
+
+  try {
+    const stdout = await subProcess.execute('sbt', ['--version'], {});
+    return stdout.split('\n')
+    .find((line) => !!line.match(/sbt script version/))!
+    .split(':')[1]
+    .trim();
+  } catch (err) {
+    debug('Failed to get sbt version sbt --version' + err.message);
+  }
+
+  // should never get here, but if it does
+  // assume sbt 1.0.0 for the purposes of copying the correct scala script and global plugin search
+  debug('Assuming sbt version is 1.0.0');
+  return '1.0.0';
+}
